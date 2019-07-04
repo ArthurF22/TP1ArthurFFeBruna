84b48a373050eb1f0ed27929523cb0c7b7140a7f
ArthurF22 <arthur132f@gmail.com>
2019-07-04 10:12:58 -0300

    inclusao do kabum a gatojava3

---
+++ GatoJava3/src/gatojava/Kabum.java
@@ -0,0 +1,160 @@
+
+package gatojava;
+/**
+     * @author Arthur França
+     */
+public class Kabum {
+        private String email;
+        private String primeironome;
+        private String sobrenome;
+        private String cpf;
+        private String rg;
+        private String dataDeNascimento;
+        private String sexo;
+        private String seustelefones;
+        private String senha;
+        private String idade;
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the primeironome
+     */
+    public String getPrimeironome() {
+        return primeironome;
+    }
+
+    /**
+     * @param primeironome the primeironome to set
+     */
+    public void setPrimeironome(String primeironome) {
+        this.primeironome = primeironome;
+    }
+
+    /**
+     * @return the sobrenome
+     */
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    /**
+     * @param sobrenome the sobrenome to set
+     */
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the rg
+     */
+    public String getRg() {
+        return rg;
+    }
+
+    /**
+     * @param rg the rg to set
+     */
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    /**
+     * @return the dataDeNascimento
+     */
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    /**
+     * @param dataDeNascimento the dataDeNascimento to set
+     */
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the seustelefones
+     */
+    public String getSeustelefones() {
+        return seustelefones;
+    }
+
+    /**
+     * @param seustelefones the seustelefones to set
+     */
+    public void setSeustelefones(String seustelefones) {
+        this.seustelefones = seustelefones;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the idade
+     */
+    public String getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+    }
+
+    
+
